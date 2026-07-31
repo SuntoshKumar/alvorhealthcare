de8a44e8b00aafbdd35dddd13c507a510d2e411c
Sam Krish <suntosemyanmar@gmail.com>
2026-07-31 09:24:53 +0630

    added top progress bar 47

--- alvorhealthcare/src/app/layout.tsx
+++ alvorhealthcare/src/app/layout.tsx
@@ -48,10 +48,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icon.png", type: "image/png", sizes: "48x48" },
-      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.svg", type: "image/svg+xml" },
     ],
-    apple: "/apple-icon.png",
   },
 };
 
